8859eee79ebe63b1cb44415e2803a12bf129b73d
insanefire10 <36336394+insanefire10@users.noreply.github.com>
2024-09-08 23:13:12 -0400

    Fixed body scaling + adding JS

---
+++ js/index.js
@@ -0,0 +1,15 @@
+const button1 = document.querySelector('.b1');
+const button3 = document.querySelector('.b3');
+const button4 = document.querySelector('.b4');
+
+button1.addEventListener('click', function(){
+    window.open('https://www.linkedin.com/in/adit-rajkumar');
+});
+
+button3.addEventListener('click', function(){
+    window.open('https://github.com/insanefire10');
+})
+
+button4.addEventListener('click', function(){
+    window.open('mailto:36336394+insanefire10@users.noreply.github.com');
+})
