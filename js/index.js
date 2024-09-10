f9b2f230aa922a29e25e8be33eaf2410fedef4a3
insanefire10 <36336394+insanefire10@users.noreply.github.com>
2024-09-10 00:58:31 -0400

    added resume

--- js/index.js
+++ js/index.js
@@ -1,10 +1,15 @@
 const button1 = document.querySelector('.b1');
+const button2 = document.querySelector('.b2');
 const button3 = document.querySelector('.b3');
 const button4 = document.querySelector('.b4');
 
 button1.addEventListener('click', function(){
     window.open('https://www.linkedin.com/in/adit-rajkumar');
 });
+
+button2.addEventListener('click', function(){
+    window.open('https://drive.google.com/file/d/1Lxk5HbyWSrDmilxIz0cQktk8sggK47J6/view?usp=sharing');
+})
 
 button3.addEventListener('click', function(){
     window.open('https://github.com/insanefire10');
